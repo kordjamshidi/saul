bb91303a5227b7684615f209647d82af2de5eaee
kordjam <kordjam@illinois.edu>
2015-11-18 20:29:01 -0600

    modified IO manager and feature generator

---
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EntityMentionRelation/SL_SVM/iJLIS2/ERIOManager.scala
@@ -0,0 +1,24 @@
+  package edu.illinois.cs.cogcomp.saulexamples.nlp.EntityMentionRelation.SL_SVM.iJLIS2
+
+  import edu.illinois.cs.cogcomp.saulexamples.EntityMentionRelation.reader.Conll04_ReaderNew
+  import edu.illinois.cs.cogcomp.sl.core.SLProblem
+
+  object ERIOManager {
+
+    def readXY(cr: Conll04_ReaderNew, l: Int, u: Int): SLProblem = {
+      var sp: SLProblem = new SLProblem()
+      var i = l
+      while (i < u) {
+        val newqi: ERqInstancePL = new ERqInstancePL(cr.relations.elementAt(i))
+        var a: Labels = new Labels()
+        val pair = cr.relations.elementAt(i)
+        a.E1Label = pair.s.sentTokens.elementAt(pair.wordId1).entType.toLowerCase
+        a.E2Label = pair.s.sentTokens.elementAt(pair.wordId2).entType.toLowerCase
+        a.RelLabel = pair.relType.toLowerCase
+        val cs: ERiStructurePL = new ERiStructurePL(newqi, a)
+        i = i + 1
+        sp.addExample(newqi,cs)
+      }
+      sp
+    }
+  }
