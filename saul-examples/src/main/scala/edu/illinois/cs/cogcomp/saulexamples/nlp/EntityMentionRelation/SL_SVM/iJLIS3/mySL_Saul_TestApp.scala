2556b816747f5e8efd542977623ed8f5db61734b
kordjam <kordjam@illinois.edu>
2016-03-25 15:08:08 -0500

    -removed java/lbjava files

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EntityMentionRelation/SL_SVM/iJLIS3/mySL_Saul_TestApp.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EntityMentionRelation/SL_SVM/iJLIS3/mySL_Saul_TestApp.scala
@@ -4,11 +4,10 @@ import edu.illinois.cs.cogcomp.saul.classifier.SL_model.JoinSLtrain
 import edu.illinois.cs.cogcomp.saulexamples.nlp.EntityMentionRelation.EntityRelationConstrainedClassifiers.perConstraintClassifier
 import edu.illinois.cs.cogcomp.saulexamples.nlp.EntityMentionRelation.EntityRelationDataModel
 
-//import edu.illinois.cs.cogcomp.saulexamples.nlp.EntityMentionRelation.RewriteBasicModel.IndependentTraining._
-
 /** Created by Parisa on 12/8/15.
   */
 object mySL_Saul_TestApp extends App {
   EntityRelationDataModel.populateWithConll()
+
   JoinSLtrain(EntityRelationDataModel, List(perConstraintClassifier))
 }
