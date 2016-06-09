66f76e1c8d09107b876e65dd03834827078bdef1
kordjam <kordjam@illinois.edu>
2016-06-09 15:52:33 -0500

    -minor edits

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EntityRelation/EntityRelationApp_SL.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EntityRelation/EntityRelationApp_SL.scala
@@ -1,25 +1,46 @@
 package edu.illinois.cs.cogcomp.saulexamples.nlp.EntityRelation
 
 import edu.illinois.cs.cogcomp.saul.classifier.ClassifierUtils
-import edu.illinois.cs.cogcomp.saul.classifier.SL_model.JoinSLtrain
-import edu.illinois.cs.cogcomp.saulexamples.nlp.EntityRelation.EntityRelationClassifiers._
+import edu.illinois.cs.cogcomp.saul.classifier.SL_model._
 import edu.illinois.cs.cogcomp.saulexamples.nlp.EntityRelation.EntityRelationConstrainedClassifiers._
-
+import edu.illinois.cs.cogcomp.saulexamples.nlp.EntityRelation.EntityRelationDataModel._
+import org.slf4j.helpers.NOPLogger
+import org.slf4j.{ Logger, LoggerFactory }
 /** Created by Parisa on 12/8/15.
   */
 object EntityRelationApp_SL extends App {
+  val logging = true
+  val logger: Logger = if (logging) LoggerFactory.getLogger(this.getClass) else NOPLogger.NOP_LOGGER;
 
   EntityRelationDataModel.populateWithConllSmallSet()
-  JoinSLtrain(EntityRelationDataModel, List(PerConstrainedClassifier, OrgConstrainedClassifier, LocConstrainedClassifier, LivesIn_PerOrg_relationConstrainedClassifier, WorksFor_PerOrg_ConstrainedClassifier))
+  val cls = List(PerConstrainedClassifier, OrgConstrainedClassifier, LocConstrainedClassifier, LivesIn_PerOrg_relationConstrainedClassifier, WorksFor_PerOrg_ConstrainedClassifier)
 
-  /* Test SL_ER */
-  println("Independent Classifiers:")
-  ClassifierUtils.TrainClassifiers(10, PersonClassifier, OrganizationClassifier, LocationClassifier, WorksForClassifier, LivesInClassifier)
-  ClassifierUtils.TestClassifiers(PersonClassifier, OrganizationClassifier, LocationClassifier, WorksForClassifier, LivesInClassifier)
-  println("SL Classifiers:")
-  PerConstrainedClassifier.test(EntityRelationDataModel.tokens.getTrainingInstances, outputGranularity = 10)
-  OrgConstrainedClassifier.test(EntityRelationDataModel.tokens.getTrainingInstances, outputGranularity = 10)
-  LocConstrainedClassifier.test(EntityRelationDataModel.tokens.getTrainingInstances, outputGranularity = 10)
-  LivesIn_PerOrg_relationConstrainedClassifier.test(EntityRelationDataModel.pairs.getTrainingInstances, outputGranularity = 10)
-  WorksFor_PerOrg_ConstrainedClassifier.test(EntityRelationDataModel.pairs.getTrainingInstances, outputGranularity = 10)
+  val testRels = pairs.getTestingInstances.toSet.toList
+  val testTokens = tokens.getTestingInstances.toSet.toList
+
+//  val SLProblem = SL_IOManager.makeSLProblem(pairs, cls)
+//  val model = Initialize(SLProblem, new SaulSLModel(cls))
+//  model.featureGenerator = new SL_FeatureGenerator(model)
+
+  val m = JoinSLtrain(pairs, List(PerConstrainedClassifier, OrgConstrainedClassifier, LocConstrainedClassifier, LivesIn_PerOrg_relationConstrainedClassifier, WorksFor_PerOrg_ConstrainedClassifier))
+
+  //  /* Test SL_ER */
+  //  println("Independent Classifiers:")
+  //  ClassifierUtils.TrainClassifiers(10, PersonClassifier, OrganizationClassifier, LocationClassifier, WorksForClassifier, LivesInClassifier)
+  //  ClassifierUtils.TestClassifiers(PersonClassifier, OrganizationClassifier, LocationClassifier, WorksForClassifier, LivesInClassifier)
+  //  println("SL Classifiers:")
+  //  PerConstrainedClassifier.test(EntityRelationDataModel.tokens.getTrainingInstances, outputGranularity = 10)
+  //  OrgConstrainedClassifier.test(EntityRelationDataModel.tokens.getTrainingInstances, outputGranularity = 10)
+  //  LocConstrainedClassifier.test(EntityRelationDataModel.tokens.getTrainingInstances, outputGranularity = 10)
+  //  LivesIn_PerOrg_relationConstrainedClassifier.test(EntityRelationDataModel.pairs.getTrainingInstances, outputGranularity = 10)
+  //  WorksFor_PerOrg_ConstrainedClassifier.test(EntityRelationDataModel.pairs.getTrainingInstances, outputGranularity = 10)
+  //
+  ClassifierUtils.TestClassifiers((testTokens, PerConstrainedClassifier), (testTokens, OrgConstrainedClassifier),
+    (testTokens, LocConstrainedClassifier))
+
+  ClassifierUtils.TestClassifiers(
+    (testRels, WorksFor_PerOrg_ConstrainedClassifier),
+    (testRels, LivesIn_PerOrg_relationConstrainedClassifier)
+  )
+
 }
