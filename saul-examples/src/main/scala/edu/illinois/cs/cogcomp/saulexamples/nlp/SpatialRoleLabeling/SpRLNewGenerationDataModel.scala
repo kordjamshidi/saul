fb6bb09a2967165cdcecb2d11e64b6b24bad9451
parisa kordjamshidi <parisakordjamshidi@parisas-MacBook-Pro.local>
2016-12-25 13:21:08 -0600

    data model using the xml reader

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/SpatialRoleLabeling/SpRLNewGenerationDataModel.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/SpatialRoleLabeling/SpRLNewGenerationDataModel.scala
@@ -1,8 +1,9 @@
 package edu.illinois.cs.cogcomp.saulexamples.nlp.SpatialRoleLabeling
 
 import edu.illinois.cs.cogcomp.saul.datamodel.DataModel
-import edu.illinois.cs.cogcomp.saulexamples.nlp.BaseTypes.{Document, Sentence, Token}
+import edu.illinois.cs.cogcomp.saulexamples.nlp.BaseTypes.{Document, Phrase, Sentence, Token}
 import edu.illinois.cs.cogcomp.saulexamples.nlp.Xml.NlpXmlReader
+
 import scala.collection.JavaConversions._
 
 /**
@@ -15,9 +16,18 @@ object SpRLNewGenerationDataModel extends DataModel {
   val documents = node[Document]
   val sentences = node[Sentence]
   val tokens = node[Token]
+  val phrases = node[Phrase]
 
   val docTosen = edge(documents,sentences)
   val sentenceToToken = edge(sentences,tokens)
+
+  val testPhraseProperty = property(phrases) {
+    x: Phrase => x.getProperty("TESTPROP_first_value")
+  }
+
+  val testDocumentProperty = property (documents) {
+    x: Document => x.getProperty("test")
+  }
 
   // when we have the annotation in the xml then we need to just use a matching sensor
  // docTosen.addSensor(a_matchingSensor)
@@ -25,41 +35,34 @@ object SpRLNewGenerationDataModel extends DataModel {
   //when we want to use our NLP tools then we use generating sensors
  // sentenceToToken.addSensor(a_generatingSensor)
 
-  val spPairs = join(trajectors,landmarks)
- // val spTriplets = join(spPairs,spatialIndicators)
-  val isTrajector= property(tokens){
-    x: Token => x.hasXmlAttribute("trajector") // or  x.hastag("trajector")
-
-      // each base type has two methods of hasXMLTag or hasXMLAttribute that recieves a string as input.
-  }
-  val lemma = property (tokens) {
-    x:Token => TextAnnotationSensor(x)
-  }
+//  val spPairs = join(tokens,tokens)
+// // val spTriplets = join(spPairs,spatialIndicators)
+//  val isTrajector= property(tokens){
+//    x: Token => x.hasXmlAttribute("trajector") // or  x.hastag("trajector")
+//
+//      // each base type has two methods of hasXMLTag or hasXMLAttribute that recieves a string as input.
+//  }
+//  val lemma = property (tokens) {
+//    x:Token => TextAnnotationSensor(x)
+//  }
 
 
 }
 
-object SpRLApp extends App{
+object SpRLApp2 extends App{
+  import SpRLNewGenerationDataModel._
+   val  reader = new NlpXmlReader("/Users/parisakordjamshidi/IdeaProjects/saul/saul-examples/src/test/resources/SpRL/2017/test.xml")
+   val documentList = reader.getDocuments("SCENE")
+   val sentencesList = reader.getSentences("SENTENCE")
+   val phrasesList = reader.getPhrases("TRAJECTOR")
+  //  reader.addPropertiesFromTag("TESTPROP", phrasesList)
 
- val reader = new NlpXmlReader("SpRL/2017/test.xml", "SCENE");
- reader.loadDocuments();
- reader.getDocuments()
- SpRLNewGenerationDataModel.documents.populate(reader.getDocuments)
- SpRLNewGenerationDataModel.sentences.populate(reader.getAllSentences)
+  documents.populate(documentList)
+  sentences.populate(sentencesList)
+  phrases.populate(phrasesList)
 
-
-
-  //since we have a matching sensor the documents and the sentences ids are martched and connected
-  //since we have a generating sensor for tokens the sentences are
-  // tokenized and connected to the contained sentences
-// we need to have a way to treat each tag as a property of the base types
-  // or as a node with a subset of nodes of the base type
-
-  SpRLNewGenerationDataModel.spPairs.populate(reader.getAllRelations("tagname"))
-  //this should get two tokens since spPairs has been defined as a pair of tokens and an edge
-  //will be established between the tokens in the relation wit the tokens in the token node
-  //we should be able to populate the same node by filtering and joining tokens
-  SpRLNewGenerationDataModel.spPairs.populate(tokens.filter(...), tokens.filter(...))
+ println( documents() prop testDocumentProperty)
+ println (phrases() prop testPhraseProperty)
 
 
 }
