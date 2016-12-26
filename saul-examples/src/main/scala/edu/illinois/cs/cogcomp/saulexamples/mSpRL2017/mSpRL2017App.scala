7efc733d890ab798f529396160188256400550c6
Umar Manzoor <umarmanzoor@gmail.com>
2016-12-26 22:39:32 +0300

    SpRL2017
    In Development Phase by Umar

---
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/mSpRL2017/mSpRL2017App.scala
@@ -0,0 +1,23 @@
+/** This software is released under the University of Illinois/Research and Academic Use License. See
+  * the LICENSE file in the root folder for details. Copyright (c) 2016
+  *
+  * Developed by: The Cognitive Computations Group, University of Illinois at Urbana-Champaign
+  * http://cogcomp.cs.illinois.edu/
+  */
+package edu.illinois.cs.cogcomp.saulexamples.mSpRL2017
+
+import edu.illinois.cs.cogcomp.saul.util.Logging
+import edu.illinois.cs.cogcomp.saulexamples.data.{DocumentReader, ImageReader}
+import edu.illinois.cs.cogcomp.saulexamples.nlp.EmailSpam.SpamClassifiers._
+
+import scala.collection.JavaConversions._
+
+object mSpRL2017App extends Logging {
+
+  val trainData = new ImageReader("C:/Users/Umar Manzoor/Documents/GitHub/saul/mSprl/images")
+
+  def main(args: Array[String]): Unit = {
+    /** Choose the experiment you're interested in by changing the following line */
+
+  }
+}
