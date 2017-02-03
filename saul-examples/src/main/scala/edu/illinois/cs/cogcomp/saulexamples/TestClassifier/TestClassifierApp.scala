a7efabda32e4031b4f18716758e1b04677afd6b9
Umar Manzoor <umarmanzoor@gmail.com>
2017-02-03 23:02:11 +0300

    Testing Classifier - Mix Features

---
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/TestClassifier/TestClassifierApp.scala
@@ -0,0 +1,20 @@
+package edu.illinois.cs.cogcomp.saulexamples.TestClassifier
+
+import TestClassifierDataModel._
+import scala.collection.JavaConversions._
+import TestClassifiers._
+
+object TestClassifierApp extends App {
+
+  val trainData = new TestClassifierReader("data/TestClassifier/train.txt").getDatafromFile()
+  val testData = new TestClassifierReader("data/TestClassifier/test.txt").getDatafromFile()
+
+  tcData.populate(trainData)
+  tcData.populate(testData, false)
+
+  TestClassifierSVM.learn(5)
+  TestClassifierSVM.test()
+
+  TestClassifierSN.learn(5)
+  TestClassifierSN.test()
+}
