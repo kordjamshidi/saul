347e1fc2bdd51a91275d44852c996c2f8c1024ce
kordjamshidi <kordjam@illinois.edu>
2017-02-04 12:45:50 -0600

    constraints minor change

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/mSpRL2017/MultiModalSpRLConstriants.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/mSpRL2017/MultiModalSpRLConstriants.scala
@@ -1,9 +1,9 @@
 package edu.illinois.cs.cogcomp.saulexamples.mSpRL2017
 
-import edu.illinois.cs.cogcomp.saul.classifier.ConstrainedClassifier
 import edu.illinois.cs.cogcomp.saulexamples.nlp.BaseTypes.Relation
 import MultiModalSpRLDataModel._
-import edu.illinois.cs.cogcomp.saulexamples.mSpRL2017.MultiModalSpRLClassifiers.{SpatialRoleClassifier, TrajectorPairClassifier}
+import edu.illinois.cs.cogcomp.saulexamples.mSpRL2017.MultiModalSpRLClassifiers.{IndicatorRoleClassifier, SpatialRoleClassifier, TrajectorPairClassifier}
+
 import scala.collection.JavaConversions._
 import edu.illinois.cs.cogcomp.saul.constraint.ConstraintTypeConversion._
 import edu.illinois.cs.cogcomp.saul.classifier.ConstrainedClassifier
@@ -11,10 +11,12 @@ import edu.illinois.cs.cogcomp.saul.classifier.ConstrainedClassifier
   * Created by parisakordjamshidi on 2/4/17.
   */
 object MultiModalSpRLConstriants {
+
 val integrity = ConstrainedClassifier.constraint[Relation]{
   x : Relation =>
 
-    ((TrajectorPairClassifier on x) is "spatialInd-trajector") ==> (SpatialRoleClassifier on (textRelations(x) ~> relationToToken) is "trajector")
-//
+    ((TrajectorPairClassifier on x) is "TR_SP") ==>
+      (SpatialRoleClassifier on (textRelations(x) ~> relationToToken) is "Trajector") and (IndicatorRoleClassifier on (textRelations(x) ~> relationToToken) is "Indicator")
+//we need to modify and use the correct edge here.
 }
 }
