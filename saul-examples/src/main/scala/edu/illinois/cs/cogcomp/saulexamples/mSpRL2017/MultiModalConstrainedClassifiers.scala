b8808b966f5e2f7fe37310d88585f943af3e6827
Taher Rahgooy <taher.rahgooy@hotmail.com>
2017-02-05 12:50:59 +0330

    bug fixes.

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/mSpRL2017/MultiModalConstrainedClassifiers.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/mSpRL2017/MultiModalConstrainedClassifiers.scala
@@ -3,15 +3,14 @@ package edu.illinois.cs.cogcomp.saulexamples.mSpRL2017
 import edu.illinois.cs.cogcomp.infer.ilp.OJalgoHook
 import edu.illinois.cs.cogcomp.saul.classifier.ConstrainedClassifier
 import edu.illinois.cs.cogcomp.saulexamples.nlp.BaseTypes.{Relation, Token}
-import MultiModalSpRLConstriants._
+import MultiModalSpRLConstraints._
 import MultiModalSpRLDataModel._
 import edu.illinois.cs.cogcomp.saulexamples.mSpRL2017.MultiModalSpRLClassifiers.{LandmarkPairClassifier, LandmarkRoleClassifier, TrajectorPairClassifier, TrajectorRoleClassifier}
 /**
   * Created by parisakordjamshidi on 2/4/17.
   */
-object MultiModalContrainedClassifiers {
+object MultiModalConstrainedClassifiers {
 
- // import SRLApps.srlDataModelObject._
   val erSolver = new OJalgoHook
 
   object TRPairConstraintClassifier extends ConstrainedClassifier[Relation, Relation](TrajectorPairClassifier) {
@@ -25,13 +24,13 @@ object MultiModalContrainedClassifiers {
     //override val pathToHead = Some()
   }
 
-  object lmConstraintClassifier extends ConstrainedClassifier[Token, Relation](LandmarkRoleClassifier) {
+  object LMConstraintClassifier extends ConstrainedClassifier[Token, Relation](LandmarkRoleClassifier) {
     def subjectTo = allConstraints
     override val solver = erSolver
     //override val pathToHead = Some()
   }
 
-  object trConstraintClassifier extends ConstrainedClassifier[Token, Relation](TrajectorRoleClassifier) {
+  object TRConstraintClassifier extends ConstrainedClassifier[Token, Relation](TrajectorRoleClassifier) {
     def subjectTo = allConstraints
     override val solver = erSolver
     //override val pathToHead = Some()
