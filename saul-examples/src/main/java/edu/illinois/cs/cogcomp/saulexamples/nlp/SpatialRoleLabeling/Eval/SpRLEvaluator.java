2f18d809530b591a7c5d9d490b556524eac7c472
Taher Rahgooy <taher.rahgooy@hotmail.com>
2017-04-18 00:52:34 +0430

    minor change

--- saul-examples/src/main/java/edu/illinois/cs/cogcomp/saulexamples/nlp/SpatialRoleLabeling/Eval/SpRLEvaluator.java
+++ saul-examples/src/main/java/edu/illinois/cs/cogcomp/saulexamples/nlp/SpatialRoleLabeling/Eval/SpRLEvaluator.java
@@ -98,8 +98,8 @@ public class SpRLEvaluator {
 
         int fp = predictedCount - tp;
         int fn = actualCount - tp;
-        double precision = tp + fp == 0 ? 0 : (double) tp / (tp + fp) * 100;
-        double recall = tp + fn == 0 ? 0 : (double) tp / (tp + fn) * 100;
+        double precision = tp == 0 ? 0 : (double) tp / (tp + fp) * 100;
+        double recall = tp == 0 ? 0 : (double) tp / (tp + fn) * 100;
         double f1 = 2 * precision * recall / (precision + recall);
 
 
